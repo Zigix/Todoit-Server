733a90b0a16586a3f1d931331d498902c5fa7e91
Zigix <olekwszk7@gmail.com>
2022-03-01 14:24:43 +0100

    Initial commit

---
+++ src/main/java/com/zigix/todoitserver/TodoitServerApplication.java
@@ -0,0 +1,13 @@
+package com.zigix.todoitserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoitServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TodoitServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zigix/todoitserver/TodoitServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zigix.todoitserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoitServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

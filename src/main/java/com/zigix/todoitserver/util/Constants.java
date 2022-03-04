1214540874b62c2f129903e3cb76f02f39128e8a
Zigix <olekwszk7@gmail.com>
2022-03-04 21:07:56 +0100

    Change verification link base path

--- src/main/java/com/zigix/todoitserver/util/Constants.java
+++ src/main/java/com/zigix/todoitserver/util/Constants.java
@@ -1,6 +1,6 @@
 package com.zigix.todoitserver.util;
 
 public final class Constants {
-    public static final String CONFIRMATION_TOKEN_LINK_PREFIX = "http://localhost:8080?token=";
+    public static final String CONFIRMATION_TOKEN_LINK_PREFIX = "http://localhost:8080/api/v1/auth/verify?token=";
     public static final String CONFIRMATION_EMAIL_SUBJECT = "Confirm your account";
 }

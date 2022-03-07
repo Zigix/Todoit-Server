d4844885a235888d64926155e9e3facde9429d3b
Zigix <olekwszk7@gmail.com>
2022-03-07 20:45:51 +0100

    Add endpoints for login and refresh token functionalities

--- src/main/java/com/zigix/todoitserver/api/AuthController.java
+++ src/main/java/com/zigix/todoitserver/api/AuthController.java
@@ -1,12 +1,17 @@
 package com.zigix.todoitserver.api;
 
+import com.zigix.todoitserver.domain.dto.AccessTokensResponse;
+import com.zigix.todoitserver.domain.dto.AuthenticationResponse;
+import com.zigix.todoitserver.domain.dto.LoginRequest;
 import com.zigix.todoitserver.domain.dto.RegisterUserRequest;
 import com.zigix.todoitserver.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 @RestController
@@ -25,5 +30,15 @@ public class AuthController {
     public ResponseEntity<String> verifyToken(@RequestParam("token") String token) {
         authService.verifyToken(token);
         return ResponseEntity.ok("Account activated");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> login(@RequestBody @Valid LoginRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+
+    @GetMapping("/refresh-token")
+    public ResponseEntity<AccessTokensResponse> refreshToken(HttpServletRequest request) {
+        return ResponseEntity.ok(authService.refreshToken(request));
     }
 }

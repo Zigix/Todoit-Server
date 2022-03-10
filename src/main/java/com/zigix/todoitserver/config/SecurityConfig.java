9ebcee0dceb951dd47d7b7876ce7be8132e190e5
Zigix <olekwszk7@gmail.com>
2022-03-10 18:53:24 +0100

    Update SecurityConfig

--- src/main/java/com/zigix/todoitserver/config/SecurityConfig.java
+++ src/main/java/com/zigix/todoitserver/config/SecurityConfig.java
@@ -1,23 +1,24 @@
 package com.zigix.todoitserver.config;
 
-import com.zigix.todoitserver.config.jwt.CustomAuthorizationFilter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.zigix.todoitserver.filter.CustomAuthenticationFilter;
+import com.zigix.todoitserver.filter.CustomAuthorizationFilter;
 import com.zigix.todoitserver.config.jwt.JwtTokenUtil;
+import com.zigix.todoitserver.domain.mapper.UserMapper;
 import com.zigix.todoitserver.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-import static org.springframework.security.config.http.SessionCreationPolicy.*;
+import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
 @EnableWebSecurity
@@ -27,6 +28,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final UserService userService;
     private final PasswordEncoder passwordEncoder;
     private final JwtTokenUtil jwtTokenUtil;
+    private final UserMapper userMapper;
+    private final ObjectMapper objectMapper;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
@@ -36,6 +39,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        CustomAuthenticationFilter customAuthenticationFilter = new CustomAuthenticationFilter(
+                authenticationManagerBean(),
+                jwtTokenUtil,
+                userMapper,
+                objectMapper
+        );
+        customAuthenticationFilter.setFilterProcessesUrl("/api/v1/auth/login");
+
         http.csrf().disable();
         http.sessionManagement().sessionCreationPolicy(STATELESS);
 
@@ -43,6 +54,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/v1/auth/**").permitAll()
                 .anyRequest().authenticated();
 
+        http.addFilterAt(customAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         http.addFilterBefore(new CustomAuthorizationFilter(jwtTokenUtil, userService),
                 UsernamePasswordAuthenticationFilter.class);
     }

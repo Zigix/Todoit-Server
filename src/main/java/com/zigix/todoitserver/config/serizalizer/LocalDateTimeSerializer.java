0f697ff807cbd096d1477f596a41ff474dc65975
Zigix <olekwszk7@gmail.com>
2022-03-10 18:52:31 +0100

    Add serializer for LocalDateTime

---
+++ src/main/java/com/zigix/todoitserver/config/serizalizer/LocalDateTimeSerializer.java
@@ -0,0 +1,20 @@
+package com.zigix.todoitserver.config.serizalizer;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class LocalDateTimeSerializer extends JsonSerializer<LocalDateTime> {
+    private final DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ISO_DATE_TIME;
+
+    @Override
+    public void serialize(LocalDateTime localDateTime,
+                          JsonGenerator jsonGenerator,
+                          SerializerProvider serializerProvider) throws IOException {
+        jsonGenerator.writeString(localDateTime.format(dateTimeFormatter));
+    }
+}

e1c2f36c70d05cedac4ef1577645173bb8ec13b3
Zigix <olekwszk7@gmail.com>
2022-03-16 18:56:12 +0100

    Revert "Add service and method for creating new Task"
    This reverts commit 7001d41d69685b224350294fe78b2f3034ef7266.

--- src/main/java/com/zigix/todoitserver/service/TaskService.java
+++
@@ -1,7 +0,0 @@
-package com.zigix.todoitserver.service;
-
-import com.zigix.todoitserver.domain.dto.CreateTaskRequest;
-
-public interface TaskService {
-    void createTask(CreateTaskRequest request);
-}

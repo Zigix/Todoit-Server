aa685e0678297560cf4a7d0abf8cca9bf949a75f
Zigix <olekwszk7@gmail.com>
2022-04-15 20:11:52 +0200

    Remove Constants class

--- src/main/java/com/zigix/todoitserver/util/Constants.java
+++
@@ -1,6 +0,0 @@
-package com.zigix.todoitserver.util;
-
-public final class Constants {
-    public static final String CONFIRMATION_TOKEN_LINK_PREFIX = "http://localhost:8080/api/v1/auth/verify?token=";
-    public static final String CONFIRMATION_EMAIL_SUBJECT = "Confirm your account";
-}

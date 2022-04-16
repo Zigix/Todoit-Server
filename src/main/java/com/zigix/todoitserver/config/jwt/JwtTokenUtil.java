1e3a5fede5142d8d72034627f1a173d1aa4780e0
Zigix <olekwszk7@gmail.com>
2022-04-16 16:39:02 +0200

    Move hardcoded strings to constant variables

--- src/main/java/com/zigix/todoitserver/config/jwt/JwtTokenUtil.java
+++ src/main/java/com/zigix/todoitserver/config/jwt/JwtTokenUtil.java
@@ -17,7 +17,10 @@ import java.util.Date;
 public class JwtTokenUtil {
     public static final int ACCESS_TOKEN_EXPIRATION_TIME_DURATION = 10;
     public static final TemporalUnit ACCESS_TOKEN_EXPIRATION_TIME_UNIT = ChronoUnit.MINUTES;
-    public static final String ACCESS_TOKEN_CLAIM_NAME_FOR_ROLES = "roles";
+    public static final String CLAIM_NAME_FOR_ROLES = "roles";
+    public static final String CLAIM_NAME_FOR_TOKEN_TYPE = "tt";
+    public static final String ACCESS_TOKEN_NAME = "access token";
+    public static final String REFRESH_TOKEN_NAME = "refresh token";
 
     private final Algorithm algorithm = Algorithm.HMAC512("secret");
     private final JWTVerifier verifier = JWT.require(algorithm).build();
@@ -29,9 +32,9 @@ public class JwtTokenUtil {
                 .withExpiresAt(Date.from(Instant.now().plus(
                         ACCESS_TOKEN_EXPIRATION_TIME_DURATION,
                         ACCESS_TOKEN_EXPIRATION_TIME_UNIT)))
-                .withClaim(ACCESS_TOKEN_CLAIM_NAME_FOR_ROLES,
+                .withClaim(CLAIM_NAME_FOR_ROLES,
                         user.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList())
-                .withClaim("TokenType", "access token")
+                .withClaim(CLAIM_NAME_FOR_TOKEN_TYPE, ACCESS_TOKEN_NAME)
                 .sign(algorithm);
     }
 
@@ -40,7 +43,7 @@ public class JwtTokenUtil {
                 .withSubject(user.getUsername())
                 .withIssuedAt(Date.from(Instant.now()))
                 .withExpiresAt(Date.from(Instant.now().plus(10, ChronoUnit.MINUTES)))
-                .withClaim("TokenType", "refresh token")
+                .withClaim(CLAIM_NAME_FOR_TOKEN_TYPE, REFRESH_TOKEN_NAME)
                 .sign(algorithm);
     }
 
@@ -56,6 +59,11 @@ public class JwtTokenUtil {
 
     public String[] getRoles(String accessToken) {
         DecodedJWT decodedJWT = verifier.verify(accessToken);
-        return decodedJWT.getClaim("roles").asArray(String.class);
+        return decodedJWT.getClaim(CLAIM_NAME_FOR_ROLES).asArray(String.class);
+    }
+
+    public String getTokenType(String token) {
+        DecodedJWT decodedJWT = verifier.verify(token);
+        return decodedJWT.getClaim(CLAIM_NAME_FOR_TOKEN_TYPE).asString();
     }
 }
